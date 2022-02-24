a1ee3651855b7c6de999ece881b89abd4be07a26
Abbi Engel <abbi.engel33@gmail.com>
2022-02-24 10:23:26 -0600

    Color swap

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 function main() {
-  // Get the "my-box" div from the document
-  // When someone clicks on the element then change the background color
-  // to red.
+  const myBox = document.getElementById('my-box');
+  myBox.addEventListener('click', event => {
+  myBox.style.backgroundColor = 'da0000';
+});
 }
 
